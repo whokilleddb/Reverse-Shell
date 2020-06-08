27f6e874302997a999f400ee57e5334879ba365a
Debjeet Banerjee <debjeetbanerjee06042001@gmail.com>
2020-06-08 19:06:05 +0530

    Update check.py

--- Client/check.py
+++ Client/check.py
@@ -4,6 +4,8 @@ def checkpass(my_socket):
      while i<3 :
          val = my_socket.recv_data()
          if val == "11fcb6aa20f1226678e8cc5cbdf8f29f94b4a6a8":
+             #Password Hash for "whokilleddb" 
+             #Generate your own password hash using hashgenerator.py and replace the given hash 
              my_socket.send_data("[+] Okay")
              counter=1 
              break
