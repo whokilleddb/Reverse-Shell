fca2e3c643e243fc629d762c086a9fcabe36dfbe
Debjeet Banerjee <debjeetbanerjee06042001@gmail.com>
2020-06-08 18:02:51 +0530

    Update check.py

--- Client/check.py
+++ Client/check.py
@@ -1,6 +1,6 @@
 def checkpass(my_socket):
      val = my_socket.recv_data()
-     if val == "11fcb6aa20f1226678e8cc5cbdf8f29f94b4a6a8":
+     if val == "11fcb6aa20f1226678e8cc5cbdf8f29f94b4a6a8": #Hash For "whokilleddb"
          my_socket.send_data("[+] Okay")
          return 1 
          
